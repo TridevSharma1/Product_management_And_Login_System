9bf587d6ef081f04dd81585406371cdd4a8ab036
TridevSharma1 <tridevx8@gmail.com>
2026-05-16 22:16:26 +0530

    url set

--- product_management/product/urls.py
+++ product_management/product/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', product_list, name='product_list'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
 
-    path('create/', views.product_create, name='product_create'),
-    path('update/<int:pk>/', views.product_update, name='product_update'),
-    path('delete/<int:pk>/', views.product_delete, name='product_delete'),
+    path('create/', product_create, name='product_create'),
+    path('update/<int:pk>/', product_update, name='product_update'),
+    path('delete/<int:pk>/', product_delete, name='product_delete'),
 
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
-    path('verify-otp/resend/', views.resend_otp, name='resend_otp'),
-    path('logout/', views.logout_view, name='custom_logout'),
-    path('profile/', views.user_profile, name='profile'),
-    path('profile/delete/', views.delete_account_request, name='delete_account_request'),
-    path('profile/delete/confirm/<uidb64>/<token>/', views.confirm_delete_account, name='confirm_delete_account'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup, name='signup'),
+    path('verify-otp/', verify_otp, name='verify_otp'),
+    path('verify-otp/resend/', resend_otp, name='resend_otp'),
+    path('logout/', logout_view, name='custom_logout'),
+    path('profile/', user_profile, name='profile'),
+    path('profile/delete/', delete_account_request, name='delete_account_request'),
+    path('profile/delete/confirm/<uidb64>/<token>/', confirm_delete_account, name='confirm_delete_account'),
 ]
